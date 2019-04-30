e19afede6fc9ffdc93889ccbdffa0afd9ba287c3
logern5 <logern5@gmail.com>
2019-04-30 08:54:18 -0500

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -1,4 +1,8 @@
 <?php
 $item = $_POST["item_code"];
-
+$handle = fopen("creds.txt","r");
+$user = fgets($handle);
+$pass = fgets($handle);
+fclose($handle);
+$conn = new mysqli($servername, $username, $password);
 ?>
