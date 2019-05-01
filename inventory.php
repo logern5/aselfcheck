e17dcb04b13765b3cc321b9bebf1506063b6e0e5
logern5 <logern5@gmail.com>
2019-05-01 08:56:55 -0500

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -8,8 +8,20 @@ $conn = new mysqli("localhost", $user, $pass);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql="";
-$result=$conn->query($sql);
+$stmt = "SELECT * FROM products WHERE id=?;";
+$stmt->bind_param("i",$item);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_array();
 $conn->close();
+echo $row["price"];
+/* TODO:
+CREATE TABLE products (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+price INT(6) NOT NULL,
+reg_date TIMESTAMP
+)
+*/
 echo "success";
 ?>
