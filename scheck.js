f7ea8651ae2e81f6a6da01a77f1b44d5b04306f2
logern5 <logern5@gmail.com>
2019-05-01 08:01:53 -0500

    Change JS debugging info

--- scheck.js
+++ scheck.js
@@ -11,15 +11,10 @@ function update_receipt(x){
 	alert(x.responseText);
 }
 function get_item(item_num){
-	try{
-	alert("OK0");
 	var x = new XMLHttpRequest();
 	var params = "item_code=" + item_num;
 	x.open("POST","inventory.php",true);
 	x.onload = function(){update_receipt(this);};
 	x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	}
-	catch(e){alert(e);}
-	alert("OK");
 	x.send();
 }
