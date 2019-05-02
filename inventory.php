e0d24e708d4a75365f660b035de1c4fd602e847b
logern5 <logern5@gmail.com>
2019-05-02 03:45:49 +0100

    fixed and tested inventory.php

--- inventory.php
+++ inventory.php
@@ -1,23 +1,27 @@
 <?php
 $item = $_POST["item_code"];
+$item = (int) 1; /*TODO: remove*/
 $handle = fopen("creds.txt","r");
 $user = rtrim(fgets($handle));
 $pass = rtrim(fgets($handle));
 fclose($handle);
-$conn = new mysqli("localhost", $user, $pass);
+$conn = new mysqli("localhost", $user, $pass, "scheck");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$stmt = "SELECT * FROM products WHERE id=?;";
+$sql = "SELECT * FROM products WHERE id = ?";
+$stmt = $conn->prepare($sql);
+if($stmt == false){
+    die("$stmt == false");
+}
 $stmt->bind_param("i",$item);
 $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_array();
-$num = $result->num_rows();
-if($rows != 1){
-    die("Rows != 1);
+$num = $result->num_rows;
+if($num != 1){
+    die("Rows != 1");
 }
 echo $row["price"];
-echo "success";
 $conn->close();
 ?>
