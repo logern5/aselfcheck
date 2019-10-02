dba3dcaa9f591dd560a566ede32a8c85d87cc44d
logern5 <logern5@gmail.com>
2019-10-02 17:57:50 -0500

    Update Check.php with debug info

--- Check.php
+++ Check.php
@@ -2,6 +2,7 @@
 $card = $_POST["card"];
 $total = $_COOKIE["total"];
 // Add actual payment system here
-header("Location: " . "/index.html");
+echo($total);
+//header("Location: " . "/index.html");
 die();
 ?>
