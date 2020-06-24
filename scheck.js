8618619e6d34b6e420ff5fc2500e3cfe97ee233b
Logan H <logern5@gmail.com>
2020-06-23 21:21:08 -0500

    Add keypad JS

--- scheck.js
+++ scheck.js
@@ -1,43 +1,30 @@
 var total = 0;
 
-function prompt_item(){
-	/* Ask user for item code, and get information from it */
-	var item_num = prompt("Enter an item code");
-	if(item_num != null){
-		alert(item_num);
-		/* Send request to DB for price */
-		get_item(item_num);
-	}
+function submit_item_code(item_code){
+	return 1234;
 }
 
-function update_receipt(x){
-	/* Update receipt text area */
-	if(x.status != 200){
-		alert("Server error");
-		return;
-	}
-	if(x.responseText.includes("Error")){
-		alert("DB Error");
-		return;
-	}
-	var toks = x.responseText.split(";");
-	var name = toks[0];
-	var price = (toks[1] / 100).toFixed(2);
-	var textarea = document.getElementById("items");
-	textarea.value += name + "\t$" + price + "\n";
-	total += toks[1] * 1;
-	document.cookie() = "total=" + (total / 100).toFixed(2);
-	//alert(total);
-	document.getElementById("total").innerHTML = (total / 100).toFixed(2);
+/* Code for numpad */
+function add_num(a){
+	var codebox = document.getElementById("codebox");
+	codebox.value += a.innerHTML + "";
 }
 
-function get_item(item_num){
-	/* Request DB for price of an item */
-	//alert(item_num);
-	var x = new XMLHttpRequest();
-	var params = "item_code=" + item_num;
-	x.open("POST","inventory.php",true);
-	x.onload = function(){update_receipt(this);};
-	x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	x.send(params);
+window.onload = function(){
+	var num_keys = document.getElementsByClassName("num");
+	for(var i = 0; i < num_keys.length; i++){
+		var num = num_keys[i].innerHTML;
+		console.log(num);
+		num_keys[i].onclick = function(){add_num(this);};
+	}
+	var clear = document.getElementById("clear");
+	clear.onclick = function(){
+		document.getElementById("codebox").value = "";
+	}
+	var enter = document.getElementById("enter");
+	enter.onclick = function(){
+		var item_code = document.getElementById("codebox").value;
+		var item_data = submit_item_code(item_code);
+		console.log("item_data: " + item_data);
+	}
 }
