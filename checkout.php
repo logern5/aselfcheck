d9be57904b4f22ec3396d27db03280c0e81d25a7
Logan H <logern5@gmail.com>
2021-07-05 14:32:58 -0600

    Improve CSS

--- checkout.php
+++ checkout.php
@@ -10,14 +10,15 @@ session_start();
   <meta name="viewport" content="width=device-width" />
   <title>SelfCheck</title>
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+  <link rel="stylesheet" href="main.css" />
 </head>
-<body>
+<body class="w3-light-gray">
   <h1>aselfcheck</h1>
-  <div class="w3-container">
+  <div class="w3-container ccdiv">
     Total: $ <?php echo(number_format($_SESSION["total"],2)); ?>
     <form action="/check.php" method="POST">
-      Card no: <input type="tel" name="card" maxlength="19" /><br/>
-      <input type="submit" value="Pay" />
+      Card no: <input type="tel" name="card" maxlength="19" class="card-input" /><br/><br/>
+      <input type="submit" class="w3-button sbtn w3-center" value="Pay" />
     </form>
   </div>
 </body>
--- index.php
+++ index.php
@@ -5,17 +5,18 @@ $_SESSION["total"] = 0;
 ?>
 
 <!DOCTYPE html>
-<html>
+<html class="w3-light-gray">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width" />
   <title>SelfCheck</title>
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" />
+  <link rel="stylesheet" href="main.css" />
 </head>
 <body>
   <h1>aselfcheck</h1>
   <div class="w3-container">
-    <a class="w3-button w3-green w3-padding w3-display-middle" href="/scheck.html">Start</a>
+    <a class="w3-button w3-green w3-padding w3-display-middle cout" href="/scheck.html">Check Out</a>
   </div>
 </body>
 </html>
